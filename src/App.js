f0167f63aa8dfde0d3cfe77b5b0a8578ae3fdfa3
Nick Miller <nicmi@amazon.com>
2020-01-02 14:07:31 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+	constructor(props) { 
+		super(props);
+		this.roasts = ['you are a disappointment',
+				'how could you be this much of a dummy hehe'];
+		this.state = {
+			counter: 0,
+			roastMessage: ''
+		}
+		this.increaseCounter = this.increaseCounter.bind(this);
+	} 	
+
+	render() {
+	   return ( 
+	    <div className="App">
+	      <header className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+		<p>
+			{this.state.roastMessage}
+		</p>
+		<p>You've been roasted {this.state.counter} times</p> 
+		<button className='btn btn-primary btn-lg' onClick={this.increaseCounter}>Roast Me</button>
+	      </header>
+	    </div>
+	)
+	} 
+
+	increaseCounter() {
+		let index   = Math.floor(Math.random() * this.roasts.length);
+		let counter = this.state.counter;
+		let roastMessage = this.roasts[index];
+		this.setState({counter: counter + 1, roastMessage});
+	}
 }
 
 export default App;
